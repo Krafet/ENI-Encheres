8db57320d28f5fd07792e9c970bdbb4136c2dc18
Jeremy Albert <Krafet@live.fr>
2020-04-06 15:54:43 +0200

    + add Utils - dateFormat Enchere.java

---
+++ src/fr/eni/encheres/utils/Utils.java
@@ -0,0 +1,20 @@
+package fr.eni.encheres.utils;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class Utils {
+
+	
+	
+/*	public private String getDateEnchereFormat() {
+		Instant dateInstant = dateEnchere.toInstant();
+		LocalDateTime dateEnchereLdt = dateInstant.atOffset(ZoneOffset.UTC).toLocalDateTime();
+		DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		return dateEnchereLdt.format(formatDate);
+	}
+	*/
+}
