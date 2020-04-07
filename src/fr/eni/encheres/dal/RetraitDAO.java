22540f64adfc16991ed8e0e33a50947ab7feab26
Jeremy Albert <Krafet@live.fr>
2020-04-07 10:26:24 +0200

    + add JavaDoc
    CategorieDAO
    RetraitDAO

--- src/fr/eni/encheres/dal/RetraitDAO.java
+++ src/fr/eni/encheres/dal/RetraitDAO.java
@@ -8,9 +8,39 @@ import fr.eni.encheres.bo.Retrait;
 
 public interface RetraitDAO {
 	
+	/**
+	 * Méthode en charge de
+	 * @author Jeremy Albert
+	 * @param retrait
+	 * @return
+	 * @throws BusinessException
+	 */
 	Retrait insert(ArticleVendu retrait) throws BusinessException;
+	
+	/**
+	 * Méthode en charge de
+	 * @author Jeremy Albert
+	 * @param retrait
+	 * @return
+	 * @throws BusinessException
+	 */
 	boolean update(ArticleVendu retrait) throws BusinessException;
+	
+	/**
+	 * Méthode en charge de
+	 * @author Jeremy Albert
+	 * @param retrait
+	 * @return
+	 * @throws BusinessException
+	 */
 	boolean delete(ArticleVendu retrait) throws BusinessException;
+	
+	/**
+	 * Méthode en charge de
+	 * @author Jeremy Albert
+	 * @return
+	 * @throws BusinessException
+	 */
 	List<Retrait> selectAll() throws BusinessException;
 	
 }
