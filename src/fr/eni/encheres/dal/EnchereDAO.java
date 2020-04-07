e4e2afb2fad2d90de6ae8ac57d50ee265fb877fa
Grarvuk <grarvuk@gmail.com>
2020-04-07 17:52:03 +0200

    DAL enchère ratrapée

--- src/fr/eni/encheres/dal/EnchereDAO.java
+++ src/fr/eni/encheres/dal/EnchereDAO.java
@@ -2,12 +2,13 @@ package fr.eni.encheres.dal;
 
 import java.util.ArrayList;
 
+import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bo.Enchere;
 
 public interface EnchereDAO {
-	Enchere insert(Enchere uneEnchere);
+	boolean insert(Enchere uneEnchere) throws BusinessException;
 	boolean delete(int idUtilisateur, int idArticle);
 	boolean update(Enchere uneEnchere);
-	Enchere selectById(int idEnchere);
+	Enchere selectById(int idUtilisateur, int idArticle);
 	ArrayList<Enchere> selectAll();
 }
