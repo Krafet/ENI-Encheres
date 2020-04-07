0bc348e395579f960a628622509d9a2b755676e1
Jeremy Albert <Krafet@live.fr>
2020-04-07 10:12:10 +0200

    Ajout de BusinessException dans
    CategorieDAO
    RetraitDAO

--- src/fr/eni/encheres/dal/CategorieDAO.java
+++ src/fr/eni/encheres/dal/CategorieDAO.java
@@ -2,14 +2,15 @@ package fr.eni.encheres.dal;
 
 import java.util.List;
 
+import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bo.Categorie;
 
 public interface CategorieDAO {
 
-	Categorie insert(Categorie categorie);
-	boolean update(Categorie categorie);
-	boolean delete(Categorie categorie);
-	List<Categorie> selectAll();
+	Categorie insert(Categorie categorie) throws BusinessException;
+	boolean update(Categorie categorie) throws BusinessException;
+	boolean delete(Categorie categorie) throws BusinessException;
+	List<Categorie> selectAll() throws BusinessException;
 	
 	
 	
--- src/fr/eni/encheres/dal/RetraitDAO.java
+++ src/fr/eni/encheres/dal/RetraitDAO.java
@@ -2,14 +2,15 @@ package fr.eni.encheres.dal;
 
 import java.util.List;
 
+import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Retrait;
 
 public interface RetraitDAO {
 	
-	Retrait insert(ArticleVendu retrait);
-	boolean update(ArticleVendu retrait);
-	boolean delete(ArticleVendu retrait);
-	List<Retrait> selectAll();
+	Retrait insert(ArticleVendu retrait) throws BusinessException;
+	boolean update(ArticleVendu retrait) throws BusinessException;
+	boolean delete(ArticleVendu retrait) throws BusinessException;
+	List<Retrait> selectAll() throws BusinessException;
 	
 }
